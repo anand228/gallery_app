1c100170da212c1e6c0b639af32bbecc7031b6d0
anand228 <anandsrivastav228@gmail.com>
2021-06-19 21:32:29 +0530

    camera permission implementation

--- app/src/main/java/com/example/gallery_app/MainActivity.java
+++ app/src/main/java/com/example/gallery_app/MainActivity.java
@@ -1,13 +1,24 @@
 package com.example.gallery_app;
 
-import androidx.appcompat.app.AppCompatActivity;
-
+import android.Manifest;
+import android.content.Intent;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Toast;
+
+import com.karumi.dexter.Dexter;
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.karumi.dexter.MultiplePermissionsReport;
+import com.karumi.dexter.PermissionToken;
+import com.karumi.dexter.listener.PermissionRequest;
+import com.karumi.dexter.listener.multi.MultiplePermissionsListener;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     FloatingActionButton add, camera, gallery, share;
@@ -18,16 +29,30 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("GALLERY");
+        setView();
+        loadAnimation();
+        onClickListeners();
+    }
+
+    private void setView() {
         add = findViewById(R.id.add_button);
         camera = findViewById(R.id.camera_button);
         share = findViewById(R.id.share_button);
         gallery = findViewById(R.id.gallery_button);
+    }
 
+    private void loadAnimation() {
         rotateOpen = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_open_anim);
         rotateClose = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.rotate_close_anim);
         fromBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.from_bottom_anim);
         toBottom = AnimationUtils.loadAnimation(getApplicationContext(), R.anim.to_bottom_anim);
+    }
 
+    private void onClickListeners() {
+        // add button animation and click function
+        ////////////
+        //////////
         add.setOnClickListener(view -> {
             if (isOpen) {
                 camera.startAnimation(toBottom);
@@ -54,5 +79,39 @@ public class MainActivity extends AppCompatActivity {
 
             }
         });
+
+        // camera button permission camera external storage
+        ////////////
+        //////////
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Dexter.withContext(MainActivity.this)
+                        .withPermissions(Manifest.permission.CAMERA)
+                        .withListener(new MultiplePermissionsListener() {
+                            @Override
+                            public void onPermissionsChecked(MultiplePermissionsReport multiplePermissionsReport) {
+
+                                if(multiplePermissionsReport.areAllPermissionsGranted()){
+                                    Toast.makeText(MainActivity.this, "all permissions are granted", Toast.LENGTH_SHORT).show();
+                                    openCamera();
+                                }
+                                else Toast.makeText(MainActivity.this, "all permissions are not granted", Toast.LENGTH_SHORT).show();
+
+                            }
+
+                            @Override
+                            public void onPermissionRationaleShouldBeShown(List<PermissionRequest> list, PermissionToken permissionToken) {
+                                    permissionToken.continuePermissionRequest();
+                            }
+                        }).check();
+            }
+        });
     }
+
+    private void openCamera() {
+        Intent cam = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        startActivity(cam);
+    }
+
 }
